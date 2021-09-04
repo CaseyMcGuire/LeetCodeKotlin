0968daa5f0a91a332de1648a9a7994c9b5bc1fbd
Casey McGuire <caseyjaymcguire@gmail.com>
2021-09-04 16:19:42 -0700

    Finished problem 412.

---
+++ src/main/kotlin/problems/problem0412/Solution.kt
@@ -0,0 +1,22 @@
+package problems.problem0412
+
+class Solution {
+  fun fizzBuzz(n: Int): List<String> {
+    val answer = mutableListOf<String>()
+    for (i in 1..n) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        answer.add("FizzBuzz")
+      }
+      else if (i % 3 == 0) {
+        answer.add("Fizz")
+      }
+      else if (i % 5 == 0) {
+        answer.add("Buzz")
+      }
+      else {
+        answer.add(i.toString())
+      }
+    }
+    return answer
+  }
+}
