bd5ef765c079b7840d0b47c071e16e4fcdf5cc74
Casey McGuire <caseyjaymcguire@gmail.com>
2021-09-04 10:59:16 -0700

    Finished problem 349.

---
+++ src/main/kotlin/problems/problem0349/Solution.kt
@@ -0,0 +1,7 @@
+package problems.problem0349
+
+class Solution {
+  fun intersection(nums1: IntArray, nums2: IntArray): IntArray {
+    return nums1.toSet().intersect(nums2.toSet()).toIntArray()
+  }
+}
