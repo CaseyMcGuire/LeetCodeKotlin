90c0f1c32c91a3d41ae7df1c051d838b85d9972c
Casey McGuire <caseyjaymcguire@gmail.com>
2021-11-10 23:56:10 -0800

    Came up with one-pass solution to problem 75.

--- src/main/kotlin/problems/problem0075/Solution.kt
+++ src/main/kotlin/problems/problem0075/Solution.kt
@@ -2,34 +2,52 @@ package problems.problem0075
 
 class Solution {
   fun sortColors(nums: IntArray): Unit {
-    var first = 0
-    var second = 0
-    var third = 0
-    for (num in nums) {
-      if (num == 0) {
-        first++
+    var start = 0
+    var end = nums.size - 1
+    var i = 0
+    while (i <= end) {
+      if (i < start) {
+        i = start
       }
-      else if (num == 1) {
-        second++
+      else if (nums[i] == 0 && nums[start] == 0) {
+        i++
+        start++
       }
-      else {
-        third++
+      else if (nums[i] == 0) {
+        nums.swap(i, start)
+        start++
+      }
+      else if (nums[end] == 2) {
+        end--
+      }
+      else if (nums[i] == 2) {
+        nums.swap(i, end)
+        end--
+      }
+      else if (nums[end] == 0) {
+        nums.swap(start, end)
+        start++
+      }
+      else if (nums[i] == 2) {
+        nums.swap(i, end)
+        end--
+      }
+      else if (nums[end] == 1 && nums[i] == 1) {
+        i++
       }
     }
 
-    for (i in nums.indices) {
-      if (first > 0) {
-        nums[i] = 0
-        first--
-      }
-      else if (second > 0) {
-        nums[i] = 1
-        second--
-      }
-      else {
-        nums[i] = 2
-        third--
-      }
-    }
+
   }
+
+  private fun IntArray.swap(i: Int, j: Int) {
+    val temp = this[i]
+    this[i] = this[j]
+    this[j] = temp
+  }
+}
+
+fun main(args: Array<String>) {
+  //println(Solution().sortColors(intArrayOf(2,0,2,1,1,0)))
+  println(Solution().sortColors(intArrayOf(2,0,1)))
 }
