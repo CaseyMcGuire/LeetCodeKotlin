edd07600fc266356f1eb55a09ce6c90b7e0b57bc
Casey McGuire <caseyjaymcguire@gmail.com>
2021-12-23 22:13:51 -0800

    Finished problem 27 (better).

--- src/main/kotlin/problems/problem0027/Solution.kt
+++ src/main/kotlin/problems/problem0027/Solution.kt
@@ -3,16 +3,14 @@ package problems.problem0027
 class Solution {
   fun removeElement(nums: IntArray, `val`: Int): Int {
     var numOccurrences = 0
-    for (num in nums) {
-      if (num == `val`) {
-        numOccurrences++
-      }
-    }
     var cur = 0
     for (i in nums.indices) {
       if (nums[i] != `val`) {
         nums[cur] = nums[i]
         cur++
+      }
+      else {
+        numOccurrences++
       }
     }
     return nums.size - numOccurrences
