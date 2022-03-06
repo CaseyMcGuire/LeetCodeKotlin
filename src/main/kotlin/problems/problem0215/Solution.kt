dc5b441733a91765bb3946ade478c720f4e4ae96
Casey McGuire <caseyjaymcguire@gmail.com>
2022-03-06 14:44:31 -0800

    Fixed bug in problem 215

--- src/main/kotlin/problems/problem0215/Solution.kt
+++ src/main/kotlin/problems/problem0215/Solution.kt
@@ -3,6 +3,6 @@ package problems.problem0215
 class Solution {
   fun findKthLargest(nums: IntArray, k: Int): Int {
     val sortedNums = nums.sorted()
-    return sortedNums[sortedNums.size - k + 1]
+    return sortedNums[sortedNums.size - k]
   }
 }
