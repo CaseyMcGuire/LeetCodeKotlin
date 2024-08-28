750a9e54895cc2c54e03357883589a52e96dcf51
CaseyMcGuire <caseyjaymcguire@gmail.com>
2024-08-27 19:58:36 -0700

    Implemented better solution for problem 380.

--- src/main/kotlin/problems/problem0380/RandomizedSet.kt
+++ src/main/kotlin/problems/problem0380/RandomizedSet.kt
@@ -4,34 +4,32 @@ import java.util.*
 
 class RandomizedSet() {
 
-  /** Initialize your data structure here. */
-  val set = mutableSetOf<Int>()
-  val list = mutableListOf<Int>()
+  private val numToIndex = mutableMapOf<Int, Int>()
+  private val nums = mutableListOf<Int>()
 
-
-  /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
   fun insert(`val`: Int): Boolean {
-    val added = set.add(`val`)
-    if (added) {
-      list.add(`val`)
+    if (numToIndex.containsKey(`val`)) {
+      return false
     }
-    return added
+    nums.add(`val`)
+    numToIndex[`val`] = nums.size - 1
+    return true
   }
 
-  /** Removes a value from the set. Returns true if the set contained the specified element. */
   fun remove(`val`: Int): Boolean {
-    val removed = set.remove(`val`)
-    if (removed) {
-      list.remove(`val`)
+    val index = numToIndex[`val`] ?: return false
+    val lastNum = nums.removeLast()
+    numToIndex.remove(`val`)
+    if (lastNum != `val`) {
+      nums[index] = lastNum
+      numToIndex[lastNum] = index
     }
-    return removed
+    return true
   }
 
-  /** Get a random element from the set. */
   fun getRandom(): Int {
-    val random = Random()
-    val randomIndex = random.nextInt(list.size)
-    return list[randomIndex]
+    val randomIndex = (0 until nums.size).random()
+    return nums[randomIndex]
   }
 
 }
