6698b4b288de075015225e2d0f4048c4cc900d4c
CaseyMcGuire <caseyjaymcguire@gmail.com>
2024-09-02 16:38:49 -0700

    Added a better solution to problem 432. This one is actually O(1).

--- src/main/kotlin/problems/problem0432/Solution.kt
+++ src/main/kotlin/problems/problem0432/Solution.kt
@@ -4,67 +4,121 @@ import java.util.*
 
 class AllOne() {
 
-  private val stringToCount = TreeMap<String, Int>()
-  private val countToString = TreeMap<Int, MutableSet<String>>()
+  private val head = Node(-1, "")
+  private val tail = Node(-1, "")
+  private val keyToFrequencyNode = mutableMapOf<String, Node>()
+
+  init {
+    head.next = tail
+    tail.prev = head
+  }
 
   fun inc(key: String) {
-    val currentCount = stringToCount[key]
-    if (currentCount != null) {
-      val prevSet = countToString[currentCount]!!
-      prevSet.remove(key)
-      if (prevSet.isEmpty()) {
-        countToString.remove(currentCount)
+    val existingNode = keyToFrequencyNode[key]
+    if (existingNode == null) {
+      if (head.next?.count == 1) {
+        head.next!!.add(key)
+        keyToFrequencyNode[key] = head.next!!
+      }
+      else {
+        val newNode = Node(1, key)
+        keyToFrequencyNode[key] = newNode
+        head.addAfter(newNode)
       }
     }
-    stringToCount.merge(key, 1) { cur, acc -> cur + acc }
-    val count = stringToCount[key]!!
-    val set = countToString[count] ?: mutableSetOf()
-    set.add(key)
-    countToString[count] = set
+    else {
+      val next = existingNode.next
+      existingNode.remove(key)
+      keyToFrequencyNode.remove(key)
+      if (next != null && next.count == existingNode.count + 1) {
+        next.add(key)
+        keyToFrequencyNode[key] = next
+      }
+      else {
+        val newNode = Node(existingNode.count + 1, key)
+        existingNode.addAfter(newNode)
+        keyToFrequencyNode[key] = newNode
+      }
+
+      if (existingNode.isEmpty()) {
+        existingNode.detach()
+      }
+    }
   }
 
   fun dec(key: String) {
-    val currentCount = stringToCount[key]
-    if (currentCount == null) {
-      return
+    val existingNode = keyToFrequencyNode[key]
+      ?: return
+    val prev = existingNode.prev
+    existingNode.remove(key)
+    keyToFrequencyNode.remove(key)
+    if (existingNode.count > 1) {
+      if (prev != null && prev.count == existingNode.count - 1) {
+        keyToFrequencyNode[key] = prev
+        prev.add(key)
+      }
+      else {
+        val newNode = Node(existingNode.count - 1, key)
+        keyToFrequencyNode[key] = newNode
+        prev!!.addAfter(newNode)
+      }
     }
-    val curSet = countToString[currentCount]!!
-    curSet.remove(key)
-    if (curSet.isEmpty()) {
-      countToString.remove(currentCount)
-    }
-    val newCount = currentCount - 1
-    if (newCount == 0) {
-      stringToCount.remove(key)
-    }
-    else {
-      stringToCount[key] = newCount
-      val set = countToString[newCount] ?: mutableSetOf()
-      set.add(key)
-      countToString[newCount] = set
+
+    if (existingNode.isEmpty()) {
+      existingNode.detach()
     }
   }
 
   fun getMaxKey(): String {
-    val last = countToString.lastEntry()
-    if (last == null) {
-      return ""
-    }
-    for (elem in last.value) {
-      return elem
-    }
-    return ""
+    return tail.prev?.getElement() ?: ""
   }
 
   fun getMinKey(): String {
-    val first = countToString.firstEntry()
-    if (first == null) {
-      return ""
-    }
-    for (elem in first.value) {
-      return elem
-    }
-    return ""
+    return head.next?.getElement() ?: ""
   }
 
+}
+
+data class Node(val count: Int, val firstString: String) {
+  var next: Node? = null
+  var prev: Node? = null
+  val strs = mutableSetOf<String>(firstString)
+
+  fun detach() {
+    prev?.next = next
+    next?.prev = prev
+    prev = null
+    next = null
+  }
+
+  fun addAfter(node: Node) {
+    val oldNext = next
+    next = node
+    oldNext?.prev = node
+    node.next = oldNext
+    node.prev = this
+  }
+
+  fun add(str: String) {
+    strs.add(str)
+  }
+
+  fun remove(str: String) {
+    strs.remove(str)
+  }
+
+  fun isEmpty(): Boolean {
+    return strs.isEmpty()
+  }
+
+  fun isValid(): Boolean {
+    return count != -1
+  }
+
+  fun getElement(): String? {
+    for (elem in strs) {
+      return elem
+    }
+    return null
+  }
 }
