158e308ea02a03095fdf6edba34571e9d68b39ca
CaseyMcGuire <caseyjaymcguire@gmail.com>
2025-07-19 16:18:59 -0700

    Add better solution for problem 904

--- src/main/kotlin/problems/problem0904/Solution.kt
+++ src/main/kotlin/problems/problem0904/Solution.kt
@@ -1,44 +1,43 @@
 package problems.problem0904
 
 class Solution {
-  fun totalFruit(tree: IntArray): Int {
-    var currentIndex = 0
-    var currentMax = 0
-    var runningCount = 0
-    var currentRunningFruit = CurrentFruit(0, tree[0])
-    var fruits = mutableSetOf<Int>()
-    fruits.add(tree[0])
-    while (currentIndex < tree.size) {
-      val currentFruit = tree[currentIndex]
-      val containsCurrentFruit = fruits.contains(currentFruit)
-      // we found a new fruit
-      if (fruits.size == 2 && !containsCurrentFruit) {
-        if (currentMax < runningCount) {
-          currentMax = runningCount
-        }
-        runningCount = 0
-        currentIndex = currentRunningFruit.startingIndex
-        fruits = mutableSetOf(currentRunningFruit.fruit, currentFruit)
+  fun totalFruit(fruits: IntArray): Int {
+    val fruitToFrequency = mutableMapOf<Int, Int>()
+    var i = fruits.size - 1
+    var j = fruits.size - 1
+    var maxSoFar: Int? = null
+    while (true) {
+      if (fruitToFrequency.size > 2) {
+        fruitToFrequency.decrement(fruits[j])
+        j--
+        continue
       }
-      else {
-        if (currentRunningFruit.fruit != currentFruit) {
-          currentRunningFruit = CurrentFruit(currentIndex, currentFruit)
-        }
-        runningCount++
-        fruits.add(currentFruit)
-        currentIndex++
+      val curSize = j - i
+      if (maxSoFar == null || maxSoFar < curSize) {
+        maxSoFar = curSize
       }
+
+      if (i < 0) {
+        break
+      }
+      fruitToFrequency.increment(fruits[i])
+      i--
     }
-    return if (runningCount > currentMax) {
-      runningCount
-    } else {
-      currentMax
+    return maxSoFar!!
+  }
+
+  private fun MutableMap<Int, Int>.increment(num: Int) {
+    this.merge(num, 1) { cur, acc -> cur + acc }
+  }
+
+  private fun MutableMap<Int, Int>.decrement(num: Int) {
+    val frequency = this[num] ?: num
+    if (frequency == 1) {
+      this.remove(num)
+    }
+    else {
+      this[num] = frequency - 1
     }
   }
-}
 
-data class CurrentFruit(val startingIndex: Int, val fruit: Int)
-
-fun main(args: Array<String>) {
-  println(Solution().totalFruit(intArrayOf(0,1,2,2)))
 }
