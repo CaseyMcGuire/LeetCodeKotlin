82dda5b6bd3ace9cf32752e69a02eb5c016a39c9
CaseyMcGuire <caseyjaymcguire@gmail.com>
2025-07-20 17:16:17 -0700

    Finished problem 247.

--- src/main/kotlin/problems/problem0247/Solution.kt
+++ src/main/kotlin/problems/problem0247/Solution.kt
@@ -4,46 +4,46 @@ import java.util.*
 
 class Solution {
   fun findStrobogrammatic(n: Int): List<String> {
-    val nums = mutableListOf<String>()
-    val possibleChoices = listOf('0', '1', '6', '8', '9')
-    fun recurse(curString: LinkedList<Char>) {
-      if (curString.size == n) {
-        if (n > 1 && curString[0] == '0') {
-          return
-        }
-        nums.add(curString.joinToString(""))
+    val flippableNums = mutableSetOf<Char>('0', '1', '6', '8', '9')
+    val strobogrammaticNumbers = mutableListOf<String>()
+
+    val curNum = MutableList<Char>(n) { '0' }
+    fun recurse(i: Int, j: Int) {
+      if (i > j) {
+        strobogrammaticNumbers.add(curNum.joinToString(""))
         return
       }
-      for (choice in possibleChoices) {
-        if (choice == '6') {
-          curString.addFirst('6')
-          curString.addLast('9')
+      else if (i == j) {
+        for (num in flippableNums) {
+          if (num == getRotatedNum(num)) {
+            curNum[i] = num
+            strobogrammaticNumbers.add(curNum.joinToString(""))
+          }
         }
-        else if (choice == '9') {
-          curString.addFirst('9')
-          curString.addLast('6')
-        }
-        else {
-          curString.addFirst(choice)
-          curString.addLast(choice)
-        }
-        recurse(curString)
-        curString.removeFirst()
-        curString.removeLast()
+        return
       }
-    }
-    if (n % 2 == 0) {
-      recurse(LinkedList())
-    }
-    else {
-      for (choice in possibleChoices) {
-        if (choice == '6' || choice == '9') {
+
+      for (num in flippableNums) {
+        if (i == 0 && num == '0') {
           continue
         }
-        recurse(LinkedList(listOf(choice)))
+        curNum[i] = num
+        curNum[j] = getRotatedNum(num)
+        recurse(i + 1, j - 1)
       }
     }
+    recurse(0, n - 1)
+    return strobogrammaticNumbers
+  }
 
-    return nums
+  private fun getRotatedNum(num: Char): Char {
+    return when (num) {
+      '0' -> '0'
+      '1' -> '1'
+      '6' -> '9'
+      '8' -> '8'
+      '9' -> '6'
+      else -> throw IllegalArgumentException("Can't rotate $num")
+    }
   }
 }
