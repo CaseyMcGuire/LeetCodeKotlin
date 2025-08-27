7edaf72618b3fba2f21ac52409702e735ad3f9be
CaseyMcGuire <caseyjaymcguire@gmail.com>
2025-08-26 22:29:05 -0700

    Finished problem 91. (Better solution)

--- src/main/kotlin/problems/problem0091/Solution.kt
+++ src/main/kotlin/problems/problem0091/Solution.kt
@@ -2,77 +2,32 @@ package problems.problem0091
 
 class Solution {
   fun numDecodings(s: String): Int {
-    if (s.isEmpty()) {
+    if (s.startsWith("0")) {
       return 0
     }
-    if (s.length == 1) {
-      return if (getMapping(s[0].toString()) != null) 1 else 0
-    }
-    if (s.length > 1 && s[0] == '0') {
-      return 0
-    }
+    val cache = mutableMapOf<Int, Int>()
 
-    val decodings = IntArray(s.length)
-
-    for (i in decodings.size - 1 downTo 0) {
-      if (s[i] == '0') {
-        decodings[i] = 0
-        continue
+    fun recurse(i: Int): Int {
+      if (i == s.length) {
+        return 1
       }
-      val firstMapping = getMapping(s[i].toString())
-      if (firstMapping != null) {
-        if (i == decodings.size - 1) {
-          decodings[i] = 1
-          continue
-        }
-        else {
-          decodings[i] = decodings[i + 1]
-        }
+      else if (i >= s.length) {
+        return 0
+      }
+      val cachedValue = cache[i]
+      if (cachedValue != null) {
+        return cachedValue
       }
 
-      val secondMappings = getMapping(s[i].toString() + s[i + 1].toString())
-      if (secondMappings != null) {
-        if (i == decodings.size - 2) {
-          decodings[i] = decodings[i + 1] + 1
-        }
-        else {
-          decodings[i] = decodings[i + 2] + decodings[i + 1]
-        }
+      var result = if (s[i] != '0') recurse(i + 1) else 0
+      val c = s[i]
+      val isDoubleDigit = c == '1' || (c == '2' && s.getOrNull(i + 1) in '0'..'6')
+      if (isDoubleDigit) {
+        result += recurse(i + 2)
       }
+      cache[i] = result
+      return result
     }
-
-    return decodings[0]
-  }
-
-  private fun getMapping(s: String): String? {
-    return when(s) {
-      "1" -> "A"
-      "2" -> "B"
-      "3" -> "C"
-      "4" -> "D"
-      "5" -> "E"
-      "6" -> "F"
-      "7" -> "G"
-      "8" -> "H"
-      "9" -> "I"
-      "10" -> "J"
-      "11" -> "K"
-      "12" -> "L"
-      "13" -> "M"
-      "14" -> "N"
-      "15" -> "O"
-      "16" -> "P"
-      "17" -> "Q"
-      "18" -> "R"
-      "19" -> "S"
-      "20" -> "T"
-      "21" -> "U"
-      "22" -> "V"
-      "23" -> "W"
-      "24" -> "X"
-      "25" -> "Y"
-      "26" -> "Z"
-      else -> null
-    }
+    return recurse(0)
   }
 }
