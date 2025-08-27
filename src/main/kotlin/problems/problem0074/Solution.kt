cc8471e3bee67b8da8639bae09d25214faa0c21c
CaseyMcGuire <caseyjaymcguire@gmail.com>
2025-08-26 19:30:24 -0700

    Finished problem 74. (Better solution)

--- src/main/kotlin/problems/problem0074/Solution.kt
+++ src/main/kotlin/problems/problem0074/Solution.kt
@@ -4,9 +4,19 @@ import java.util.*
 
 class Solution {
   fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
-    for (row in matrix) {
-      if (target in row[0]..row[row.size - 1]) {
-        return Arrays.binarySearch(row, target) >= 0
+    var low = 0
+    var high = matrix.size - 1
+    while (low <= high) {
+      val middle = ((low.toLong() + high.toLong()) / 2L).toInt()
+      val row = matrix[middle]
+      if (target in row.first()..row.last()) {
+        return row.binarySearch(target) >= 0
+      }
+      else if (target > row.last()) {
+        low = middle + 1
+      }
+      else {
+        high = middle - 1
       }
     }
     return false
