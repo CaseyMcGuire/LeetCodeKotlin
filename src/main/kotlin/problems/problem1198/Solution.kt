5ebd1bb4027542ab0c099857ca90f1ed519a28f9
CaseyMcGuire <caseyjaymcguire@gmail.com>
2026-01-17 11:58:22 -0800

    Finished problem 1198.

--- src/main/kotlin/problems/problem1198/Solution.kt
+++ src/main/kotlin/problems/problem1198/Solution.kt
@@ -4,22 +4,38 @@ import java.util.*
 
 class Solution {
   fun smallestCommonElement(mat: Array<IntArray>): Int {
-    var intersection: Set<Int>? = null
-    for (row in mat.indices) {
-      val currentRow = mutableSetOf<Int>()
-      for (column in mat[row].indices) {
-        currentRow.add(mat[row][column])
-      }
-      if (intersection == null) {
-        intersection = currentRow
-      }
-      else {
-        intersection = intersection.intersect(currentRow)
-      }
+    var largest = 0
+    val frontPq = PriorityQueue<Coordinate>(compareBy({ mat[it.i][it.j] }))
+    for (i in mat.indices) {
+      val coordinate = Coordinate(i, 0)
+      val value = mat[i][0]
+      largest = Math.max(largest, value)
+      frontPq.add(coordinate)
     }
-    if (intersection == null || intersection.isEmpty()) {
-      return -1
+
+    while (true) {
+      val top = frontPq.poll()
+      val topNum = mat[top.i][top.j]
+      if (largest == topNum) {
+        return topNum
+      }
+
+      val next = top.getNext()
+      val outOfBounds = next.j == mat[next.i].size
+      if (outOfBounds) {
+        break
+      }
+      val nextNum = mat[next.i][next.j]
+      largest = Math.max(largest, nextNum)
+      frontPq.add(next)
     }
-    return TreeSet(intersection!!).pollFirst()
+    return -1
+  }
+
+}
+
+data class Coordinate(val i: Int, val j: Int) {
+  fun getNext(): Coordinate {
+    return Coordinate(i, j + 1)
   }
 }
