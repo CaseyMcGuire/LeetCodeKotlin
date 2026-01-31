26a250e48954c2016eae7ecbab00c912dc125d59
CaseyMcGuire <caseyjaymcguire@gmail.com>
2026-01-30 17:40:44 -0800

    Finished problem 531.

--- src/main/kotlin/problems/problem0531/Solution.kt
+++ src/main/kotlin/problems/problem0531/Solution.kt
@@ -2,50 +2,31 @@ package problems.problem0531
 
 class Solution {
   fun findLonelyPixel(picture: Array<CharArray>): Int {
-    if (picture.isEmpty() || picture[0].isEmpty()) {
-      return -1
+    val colToNumBlackPixels = mutableMapOf<Int, Int>()
+    val singlePixelRowPoints = mutableSetOf<Point>()
+    for (i in picture.indices) {
+      val blackPoints = mutableSetOf<Point>()
+      for (j in picture[i].indices) {
+        if (picture[i][j] == 'B') {
+          blackPoints.add(Point(i, j))
+          colToNumBlackPixels.merge(j, 1) { cur, acc -> cur + acc }
+        }
+      }
+
+      if (blackPoints.size == 1) {
+        singlePixelRowPoints.addAll(blackPoints)
+      }
     }
-    val lonelyRowPixels = mutableSetOf<Coordinate>()
-    val height = picture.size
-    val width = picture[0].size
+
     var numLonelyPixels = 0
-    for (row in 0 until height) {
-      var blackPixelRow: Coordinate? = null
-      for (column in 0 until width) {
-        if (picture[row][column] == 'B') {
-          if (blackPixelRow == null) {
-            blackPixelRow = Coordinate(row, column)
-          }
-          else {
-            blackPixelRow = null
-            break
-          }
-        }
-      }
-
-      if (blackPixelRow == null) {
-        continue
-      }
-
-      var foundPixel = false
-      for (i in 0 until height) {
-        if (picture[i][blackPixelRow.j] == 'B') {
-          if (foundPixel == false) {
-            foundPixel = true
-          }
-          else {
-            foundPixel = false
-            break
-          }
-        }
-      }
-      if (foundPixel) {
+    for (point in singlePixelRowPoints) {
+      val numPixelsInColumn = colToNumBlackPixels[point.j]!!
+      if (numPixelsInColumn == 1) {
         numLonelyPixels++
       }
-
     }
     return numLonelyPixels
   }
-}
 
-data class Coordinate(val i: Int, val j: Int)
+  data class Point(val i: Int, val j: Int)
+}
