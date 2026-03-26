1239b97e18e715132d635c8f0d75af8f9c829b51
CaseyMcGuire <caseyjaymcguire@gmail.com>
2026-03-25 19:21:05 -0700

    Finished problem 662.

--- src/main/kotlin/problems/problem0662/Solution.kt
+++ src/main/kotlin/problems/problem0662/Solution.kt
@@ -4,43 +4,22 @@ import datastructures.TreeNode
 
 class Solution {
   fun widthOfBinaryTree(root: TreeNode?): Int {
+    val levelMin = mutableMapOf<Int, Int>()
+    val levelMax = mutableMapOf<Int, Int>()
     var maxSoFar = 0
-    val leftMostIndex = mutableMapOf<Int, Long>()
-    val rightMostIndex = mutableMapOf<Int, Long>()
-
-    fun recurse(node: TreeNode?, index: Long, level: Int) {
+    fun traverse(node: TreeNode?, level: Int, index: Int) {
       if (node == null) {
         return
       }
-      val value = node.`val`
-      val leftMostIndexForLevel = leftMostIndex[level]
-      if (leftMostIndexForLevel == null || leftMostIndexForLevel > index) {
-        leftMostIndex[level] = index
-      }
 
-      val rightMostIndexForLevel = rightMostIndex[level]
-      if (rightMostIndexForLevel == null || rightMostIndexForLevel < index) {
-        rightMostIndex[level] = index
-      }
+      val newMax = levelMax.merge(level, index) { cur, acc -> Math.max(cur, acc) }!!
+      val newMin = levelMin.merge(level, index) { cur, acc -> Math.min(cur, acc) }!!
+      maxSoFar = Math.max(maxSoFar, Math.abs(newMax - newMin) + 1)
 
-      val leftChildIndex = index * 2L
-      val rightChildIndex = index * 2L + 1L
-      recurse(node?.left, leftChildIndex, level + 1)
-      recurse(node?.right, rightChildIndex, level + 1)
+      traverse(node.left, level + 1, (index * 2) - 1)
+      traverse(node.right, level + 1, index * 2)
     }
-    recurse(root, 0, 0)
-
-    var curLevel = 0
-    var curMax = 0L
-    while (true) {
-      val leftMost = leftMostIndex[curLevel]
-      val rightMost = rightMostIndex[curLevel]
-      if (leftMost == null || rightMost == null) {
-        return curMax.toInt()
-      }
-      curMax = Math.max(curMax, (rightMost - leftMost + 1).toLong())
-      curLevel++
-    }
-    return curMax.toInt()
+    traverse(root, 0, 1)
+    return maxSoFar
   }
 }
